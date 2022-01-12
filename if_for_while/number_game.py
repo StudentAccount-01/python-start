5592a36997312d8af3584d13d81d6ba08959af08
Michael <mihail.tchernov@yandex.ru>
2022-01-12 19:21:25 +0300

    Добавлена игра Угадай число

--- if_for_while/number_game.py
+++ if_for_while/number_game.py
@@ -1,16 +1,26 @@
-import random
-
-
-NUMBER_LIMIT = 0, 10
-
-
-def generate_number() -> int:
-    return random.randint(*NUMBER_LIMIT)
-
-
 if __name__ == '__main__':
-    unknown_number = generate_number()
-    
-    # Код пишем только после этой строки
-    
+    _ = input('Загадай число и нажми клавишу Enter')
 
+    min_number = 1
+    max_number = 100
+    step_count = 0
+    while max_number - min_number > 1:
+        mean_number = (min_number + max_number) // 2
+        
+        user_answer = input(f'Твое число меньше {mean_number}? (y/n): ')
+        if user_answer == 'y':
+            max_number = mean_number
+            step_count += 1
+        elif user_answer == 'n':
+            min_number = mean_number
+            step_count += 1
+        else:
+            print('Введен неправильный символ. Повторите ответ на вопрос еще раз')
+    else:
+        user_answer = input(f'Твое число - {min_number}? (y/n): ')
+        step_count += 1
+        if user_answer == 'n':
+            print(f'Твое число - {max_number}')
+        else:
+            print(f'Твое число - {min_number}')
+        print(f'Количество ходов - {step_count}')
